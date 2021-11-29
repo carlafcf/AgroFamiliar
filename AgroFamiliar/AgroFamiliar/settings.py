f69939c23ba6c275c67a5abbbe49a8db0cea2a27
Carla Fernandes <carlafcf@gmail.com>
2021-11-29 20:50:15 -0300

    Base das telas criado.

--- AgroFamiliar/AgroFamiliar/settings.py
+++ AgroFamiliar/AgroFamiliar/settings.py
@@ -17,6 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'Templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'Static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
